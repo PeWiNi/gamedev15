f5f668c83b0d867ef0b1514698a6ea3d8aa9c716
GGBuzz <marcwgj@hotmail.com>
2015-02-12 13:25:09 +0100

    simple networking impl

---
+++ MMO/Assets/Scripts/Networking/SpawnPlayerObject02.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class SpawnPlayerObject02 : MonoBehaviour
+{
+
+		public Transform LANPlayer;
+		public ArrayList playerScripts = new ArrayList ();
+
+		void OnServerInitialized ()
+		{
+				SpawnPlayer (Network.player);
+		}
+
+		void OnPlayerConnected (NetworkPlayer player)
+		{
+				SpawnPlayer (player);
+		}
+
+		void SpawnPlayer (NetworkPlayer player)
+		{
+				string tempPlayerString = player.ToString ();
+				int playerNumber = Convert.ToInt32 (tempPlayerString);
+				Transform newPlayerTransform = (Transform)Network.Instantiate (LANPlayer, transform.position, transform.rotation, playerNumber);
+				playerScripts.Add (newPlayerTransform.GetComponent ("LANObject01"));
+				NetworkView theNetworkView = newPlayerTransform.networkView;
+				theNetworkView.RPC ("SetPlayer", RPCMode.AllBuffered, player);
+		}
+}
