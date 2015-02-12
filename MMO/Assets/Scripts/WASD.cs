44a80be3c7bb47c1f4baa487fbdac4adbf8048ba
Mats Stenhaug <matsstenhaug@hotmail.com>
2015-02-12 13:18:35 +0100

    added ball and rotation to moving

--- MMO/Assets/Scripts/WASD.cs
+++ MMO/Assets/Scripts/WASD.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class WASD : MonoBehaviour {
 
+	/*TODO: Make state controller, so you don't connect/split during combat.
+	 * 
+	 * */
 	
 	public KeyCode moveUp;// = KeyCode.W;
 	public KeyCode moveDown;// = KeyCode.S;
@@ -19,6 +22,7 @@ public class WASD : MonoBehaviour {
 	Vector3 camPos;
 	private Vector3 gravity;
 	private Quaternion rotation;
+	private int currentRotationFace;
 	// Use this for initialization
 	void Start () {
 	
@@ -32,15 +36,70 @@ public class WASD : MonoBehaviour {
 
 	void jump(){
 			gravity.y = jumpHeight;
-		rigidbody.velocity = gravity;
+			rigidbody.velocity = gravity;
 			jumping = true;
 	}
+
+	void setRotation(bool up, bool down, bool left, bool right){
+	
+		if (up && right) {
+			currentRotationFace = 45;
+			//transform.rotation = Quaternion.AngleAxis(45 ,Vector3.up);//rotation;
+		}else if(up && left){
+			currentRotationFace = 315;
+			//transform.rotation = Quaternion.AngleAxis(315 ,Vector3.up);//rotation;
+		}else if(up){
+			currentRotationFace = 0;
+			//transform.rotation = Quaternion.AngleAxis(0 ,Vector3.up);//rotation;
+		}else if(down && left){
+			currentRotationFace = 225;
+			//transform.rotation = Quaternion.AngleAxis(225 ,Vector3.up);//rotation;
+		}else if(down && right){
+			currentRotationFace = 135;
+			//transform.rotation = Quaternion.AngleAxis(135 ,Vector3.up);//rotation;
+		}else if(down){
+			currentRotationFace = 180;
+			//transform.rotation = Quaternion.AngleAxis(180 ,Vector3.up);//rotation;
+		}else if(right){
+			currentRotationFace = 90;
+			//transform.rotation = Quaternion.AngleAxis(90 ,Vector3.up);//rotation;
+		}else if(left){
+			currentRotationFace = 270;
+			//transform.rotation = Quaternion.AngleAxis(270 ,Vector3.up);//rotation;
+		}
+
+			transform.rotation = Quaternion.AngleAxis(currentRotationFace ,Vector3.up);//rotation;
+		
+		//transform.rotation = Quaternion.AngleAxis(rotation.y ,Vector3.up);//rotation;
+
+	}
+	/* TODO:
+		 * Make it so that the rotation of the character changes depending on which direction 
+		 * it is going.
+		 * 8 directions -> N, NE, E, SE, S, SW, W, NW
+		 * N = 0 degrees
+		 * NE = 45 deg
+		 * E = 90 deg
+		 * SE = 135 deg
+		 * S = 180 deg
+		 * SW = 225 deg
+		 * W = 270 deg
+		 * NW = 315 deg
+		 */
 	
 	// Update is called once per frame
 	void Update () {
+		bool up, down, left, right;
+		up = false;
+		down = false;
+		left = false;
+		right = false;
+
 		keepSteady ();
+
 		position = transform.position; 
 		if (Input.GetKey (moveUp)) {
+			up = true;
 			if (Input.GetKey (sprint)) {
 				position.z += sprintspeed;
 			} else { 
@@ -51,6 +110,7 @@ public class WASD : MonoBehaviour {
 		}
 
 		if (Input.GetKey (moveDown)) {
+			down = true;
 			if(Input.GetKey(sprint)){
 				position.z -= sprintspeed;
 			}else{
@@ -61,6 +121,7 @@ public class WASD : MonoBehaviour {
 		}
 
 		if (Input.GetKey (moveRight)) {
+			right = true;
 			if(Input.GetKey (sprint)){
 				position.x += sprintspeed;
 			}else{
@@ -70,6 +131,7 @@ public class WASD : MonoBehaviour {
 			//changed = true;
 		}
 		if (Input.GetKey (moveLeft)) {
+			left = true;
 			if(Input.GetKey (sprint)){
 				position.x -= sprintspeed;
 			}else{
@@ -96,6 +158,8 @@ public class WASD : MonoBehaviour {
 			rotation.y += 10;
 			transform.rotation = Quaternion.AngleAxis(rotation.y ,Vector3.up);//
 		}
+
+
 
 		/* TODO:
 		 * Make it so that the rotation of the character changes depending on which direction 
@@ -125,6 +189,12 @@ public class WASD : MonoBehaviour {
 		camPos.y = transform.position.y + zoom ;
 		mainCam.gameObject.transform.position= camPos;
 		mainCam.gameObject.transform.LookAt (transform.position); 
+		setRotation (up, down, left, right);
+		right = false;
+		left = false;
+		up = false;
+		down = false;
+		
 	}
 	void OnCollisionEnter(Collision coll){ // Working!!
 		jumping = false;
