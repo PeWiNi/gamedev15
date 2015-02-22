504fbf47452f32eb2d9d6b7ee4e1bed7e5d6011f
GGBuzz <marcwgj@hotmail.com>
2015-02-22 18:32:11 +0100

    trying networking related stuff

--- MMO/Assets/Scripts/Networking/NetworkCallbacks.cs
+++ MMO/Assets/Scripts/Networking/NetworkCallbacks.cs
@@ -1,13 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-[BoltGlobalBehaviourAttribute]
+[BoltGlobalBehaviour]
 public class NetworkCallbacks : Bolt.GlobalEventListener
 {
+		IList<string> logMessages = new List<string> ();
+
 		public override void SceneLoadLocalDone (string map)
 		{
 				var pos = new Vector3 (Random.Range (-4, 4), 0, Random.Range (-4, 4));
 
 				BoltNetwork.Instantiate (BoltPrefabs.PlayerLANObject, pos, Quaternion.identity);
+		}
+
+		public override void OnEvent (LogEvent evnt)
+		{
+				logMessages.Insert (0, evnt.Message);
+		}
+
+		void OnGUI ()
+		{
+				int maxMessages = Mathf.Min (5, logMessages.Count);
+
+				GUILayout.BeginArea (new Rect (Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+
+				for (int i = 0; i < maxMessages; ++i) {
+						GUILayout.Label (logMessages [i]);
+				}
+
+				GUILayout.EndArea ();
 		}
 }
--- MMO/Assets/Scripts/Networking/PlayerObjectBehaviour.cs
+++ MMO/Assets/Scripts/Networking/PlayerObjectBehaviour.cs
@@ -1,21 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerObjectBehaviour : Bolt.EntityBehaviour<IPlayerState>
+public class PlayerObjectBehaviour : Bolt.EntityEventListener<IPlayerState>
 {
+		public GameObject[] AbilityObjects;
+		float resetColorTime;
+		GameObject owner;
+		GameObject thisObj;
+		Vector3 start;
+		Vector3 direction;
+		GameObject boomnana;
+		Boomnana boomscript;
+		
+		void Start ()
+		{
+//				boomnana = Instantiate (Resources.Load ("Prefabs/Boomnana", typeof(GameObject)) as GameObject,
+//		                        new Vector3 (transform.position.x + 20, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+//				boomnana.GetComponent<Boomnana> ();
+		}
+
+		void Update ()
+		{
+				if (resetColorTime < Time.time) {
+						renderer.material.color = state.PlayerColor;
+				}
+		}
+
+
 		public override void Attached ()
 		{
 				state.PlayerTransform.SetTransforms (transform);
 
 				if (entity.isOwner) {
 						state.PlayerColor = new Color (Random.value, Random.value, Random.value);
+
+						for (int i = 0; i < state.AbilityArray.Length; ++i) {
+								state.AbilityArray [i].AbilityId = Random.Range (0, AbilityObjects.Length);
+						}
+						state.AbilityActiveIndex = -1;
 				}
 
 				state.AddCallback ("PlayerColor", ColorChanged);
+
+				state.AddCallback ("AbilityActiveIndex", AbilityActiveIndexChanged);
 		}
+
 
 		public override void SimulateOwner ()
 		{
+
+//				if (Input.GetKey (KeyCode.E)) {
+//						
+//						boomscript.spawn (owner, thisObj, start, direction);
+//				}
+
 				var ms = 4f;
 				var mpos = Vector3.zero;
 				if (Input.GetKey (KeyCode.W)) {
@@ -35,12 +73,46 @@ public class PlayerObjectBehaviour : Bolt.EntityBehaviour<IPlayerState>
 						transform.position = transform.position + (mpos.normalized * ms * BoltNetwork.frameDeltaTime);
 				}
 
+				if (Input.GetKeyDown (KeyCode.Alpha1))
+						state.AbilityActiveIndex = 0;
+
+				if (Input.GetKeyDown (KeyCode.Alpha2))
+						state.AbilityActiveIndex = 1;
+
+				if (Input.GetKeyDown (KeyCode.F)) {
+						var flash = FlashColorEvent.Create (entity);
+						flash.FlashColor = Color.red;
+						flash.Send ();
+				}
+
 		}
+
 
 		public void ColorChanged ()
 		{
 				renderer.material.color = state.PlayerColor;
 		}
+
+
+		public override void OnEvent (FlashColorEvent evnt)
+		{
+				resetColorTime = Time.time + 0.25f;
+				renderer.material.color = evnt.FlashColor;
+		}
+
+		void AbilityActiveIndexChanged ()
+		{
+				for (int i = 0; i < AbilityObjects.Length; ++i) {
+						AbilityObjects [i].SetActive (false);
+				}
+		
+				if (state.AbilityActiveIndex >= 0) {
+						int objectId = state.AbilityArray [state.AbilityActiveIndex].AbilityId;
+						AbilityObjects [objectId].SetActive (true);
+				}
+		}
+	
+		
 
 		void OnGUI ()
 		{
