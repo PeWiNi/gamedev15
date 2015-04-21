85b58e5ab214a363f2b686b314e2539c5cc9de2f
Tornady <clbw@itu.dk>
2015-04-21 15:31:22 +0200

    Main Menu
    Controls and Video options are designed and implemented!
    Fullscreen(windowed) (borderless) is not implemented.
    More Resolutions can be added by adding more buttons to the
    ResolutionPanel and adding them to the array in the MenuScript.

--- MMO/Assets/Scripts/MenuScript.cs
+++ MMO/Assets/Scripts/MenuScript.cs
@@ -13,6 +13,8 @@ public class MenuScript : MonoBehaviour
     GameObject AudioMenu;
     GameObject VideoMenu;
     GameObject ControlsMenu;
+    GameObject ResolutionPanel;
+    GameObject DisplayModePanel;
 
     public static bool hasPickedTeamOne = false;
     public static bool hasPickedTeamTwo = false;
@@ -23,15 +25,24 @@ public class MenuScript : MonoBehaviour
     string serverAddress = "";
     int serverPort = 27000;
 
+    #region Options variables
     public static float AmbientLight = 0.2f;
+    string[] resolutionList;
+
     public static float MasterSoundLevel = 1.0f;
     public static float MusicSoundLevel = 1.0f;
     public static float SFXSoundLevel = 1.0f;
 
-	// Use this for initialization
+    public static KeyCode[] KeyBindings;
+    KeyCode keyHolder = KeyCode.A;
+    Button[] KeyBindButtons;
+    #endregion
+
+    // Use this for initialization
 	void Start ()
     {
-        #region Find GameObject
+        resolutionList = new string[] {"1920x1080", "1280x720", "1024x768" };
+        #region Find GameObject(s)
         MainMenu = transform.Find("Main").gameObject;
         PlayMenu = transform.Find("Play").gameObject;
         OptionsMenu = transform.Find("Options").gameObject;
@@ -43,6 +54,41 @@ public class MenuScript : MonoBehaviour
         VideoMenu = transform.Find("Video").gameObject;
         ControlsMenu = transform.Find("Controls").gameObject;
 
+        ResolutionPanel = GameObject.Find("ResolutionPanel");
+        DisplayModePanel = GameObject.Find("DisplayPanel");
+        /*
+        foreach (string text in resolutionList) //Failed attempt of making a dynamic list of possible resolutions (they are too small and always apply the text form last generated to buttonText)
+        {
+            Button btn = Instantiate((GameObject)Resources.Load("Prefabs/UIButton")).GetComponent<Button>();
+            btn.transform.parent = ResolutionPanel.transform;
+            btn.GetComponentInChildren<Text>().text = text;
+            btn.onClick.AddListener(() => { resolution(text); });
+        }
+         */
+        Button[] resolutions = ResolutionPanel.GetComponentsInChildren<Button>();
+        int i = 0;
+        foreach (Button btn in resolutions)
+        {
+            try
+            {
+                string text = resolutionList[i];
+                btn.GetComponentInChildren<Text>().text = text;
+                btn.onClick.AddListener(() => { resolution(text); });
+            }
+            catch (System.Exception e)
+            {
+               btn.gameObject.SetActive(false);
+            }
+            i++;
+        }
+        KeyBindButtons = ControlsMenu.GetComponentsInChildren<Button>();
+
+        ResolutionPanel.SetActive(false);
+        DisplayModePanel.SetActive(false);
+        #endregion
+
+
+        #region Disable all Menus but Main
         MainMenu.SetActive(true);
         PlayMenu.SetActive(false);
         OptionsMenu.SetActive(false);
@@ -52,6 +98,11 @@ public class MenuScript : MonoBehaviour
         VideoMenu.SetActive(false);
         ControlsMenu.SetActive(false);
         #endregion
+    }
+
+    void resolution(string s)
+    {
+        GameObject.Find("CurrentResolutionText").GetComponent<Text>().text = s;
     }
 
     void Awake()
@@ -68,7 +119,7 @@ public class MenuScript : MonoBehaviour
         //Sound Settings Update(s)
         AudioListener.volume = MasterSoundLevel;
         //TODO: Add Music controller and set volume
-        GameObject.Find("SoundController").GetComponent<SoundController>().getSoundPlayer().volume = SFXSoundLevel; //Example of how to set the SFX - dunno if works in practice
+        //GameObject.Find("SoundController").GetComponent<SoundController>().getSoundPlayer().volume = SFXSoundLevel; //Example of how to set the SFX - dunno if works in practice
     }
 
     #region GUI Functions
@@ -141,6 +192,7 @@ public class MenuScript : MonoBehaviour
     #region Bolt Server Functions
     public void StartServer()
     {
+        makeKeyBindings();
         foreach (string value in BoltScenes.AllScenes) 
             map = value;
         BoltLauncher.StartServer(new UdpEndPoint(UdpIPv4Address.Any, (ushort)serverPort));
@@ -148,6 +200,7 @@ public class MenuScript : MonoBehaviour
     }
     void StartClient()
     {
+        makeKeyBindings();
         BoltLauncher.StartClient(UdpEndPoint.Any);
         BoltNetwork.Connect(new UdpEndPoint(UdpIPv4Address.Parse(serverAddress), (ushort)serverPort));
     }
@@ -172,10 +225,12 @@ public class MenuScript : MonoBehaviour
     #endregion
 
     #region Options Functions
+    //Video
     public void Brightness()
     {
         AmbientLight = GameObject.Find("BrightnessSlider").GetComponent<Slider>().value;
     }
+    //Audio
     public void MasterVolume()
     {
         MasterSoundLevel = GameObject.Find("MasterSlider").GetComponent<Slider>().value;
@@ -188,8 +243,52 @@ public class MenuScript : MonoBehaviour
     {
         MasterSoundLevel = GameObject.Find("SFXSlider").GetComponent<Slider>().value;
     }
+    //Controls
+    public void KeyBind(Button b)
+    {
+        b.GetComponentInChildren<Text>().text = keyHolder.ToString();
+    }
+
+    public void SetResolution() //Borderless not currently supported
+    {
+        //Input values
+        string resolution = GameObject.Find("CurrentResolutionText").GetComponent<Text>().text;
+        string display = GameObject.Find("CurrentDisplayText").GetComponent<Text>().text;
+
+        bool fullscreen;
+        string[] resol = resolution.Split(char.Parse("x"));
+        switch (display)
+        {
+            case "FullScreen":
+                fullscreen = true;
+                break;
+            default:
+                fullscreen = false;
+                break;
+        }
+        Debug.Log("resolution = " + int.Parse(resol[0]) + " by " + int.Parse(resol[1]) + " fullscreen = " + fullscreen);
+        Screen.SetResolution(int.Parse(resol[0]), int.Parse(resol[1]), fullscreen);
+    }
     #endregion
 
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey) {
+            keyHolder = e.keyCode;
+            Debug.Log("Detected key code: " + e.keyCode);
+        }
+    }
+    KeyCode[] makeKeyBindings()
+    {
+        KeyCode TailSlap    = (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyBindButtons[1].GetComponentInChildren<Text>().text);
+        KeyCode BOOMnana    = (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyBindButtons[2].GetComponentInChildren<Text>().text);
+        KeyCode BananaPuke  = (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyBindButtons[3].GetComponentInChildren<Text>().text);
+        KeyCode FishSlam    = (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyBindButtons[4].GetComponentInChildren<Text>().text);
+        KeyCode Heal        = (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyBindButtons[5].GetComponentInChildren<Text>().text);
+
+        return KeyBindings = new KeyCode[] { TailSlap, BOOMnana, BananaPuke, FishSlam, Heal };
+    }
     public void Exit()
     {
         Application.Quit();
