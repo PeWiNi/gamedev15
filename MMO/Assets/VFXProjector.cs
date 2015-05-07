b8d166182ef6e6d29284e21f90ec7c52fb691ec5
Tornady <clbw@itu.dk>
2015-05-07 13:46:09 +0200

    Projector timing fixes
    Now only the Boomnana-key keeps the timer from counting!

--- MMO/Assets/VFXProjector.cs
+++ MMO/Assets/VFXProjector.cs
@@ -33,20 +33,20 @@ public class VFXProjector : MonoBehaviour
 	void Update () {
         if (timer <= 0) {
             projector.enabled = false;
-        } else if (!Input.anyKey) {
+        } else if (!Input.GetKey(MenuScript.KeyBindings[1])) {
             timer -= Time.deltaTime;
         }
         if (Input.GetKeyDown(MenuScript.KeyBindings[0])) { //Tail
-            castProjection(tailAim, 0.35f, 65, 5, 1.5f);
+            castProjection(tailAim, 0.35f, 65, 5, .5f);
         }
         if (Input.GetKeyDown(MenuScript.KeyBindings[1])) { //Boomnana
             castProjection(boomAim, 0.2f, 220, 15, 3);
         }
         if (Input.GetKeyDown(MenuScript.KeyBindings[2])) { //Puke
-            castProjection(pukeAim, 1, 60, 5, 4);
+            castProjection(pukeAim, 1, 60, 5, 3);
         }
         if (Input.GetKeyDown(MenuScript.KeyBindings[3])) { //Fish
-            castProjection(fishAim, 1, 40, 2, 2);
+            castProjection(fishAim, 1, 40, 2, 1.5f);
         }
 	}
     void castProjection(Texture txt, float aRatio, float height, float distance, float activeTime) {
