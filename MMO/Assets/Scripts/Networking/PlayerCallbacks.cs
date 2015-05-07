a29585b4ab7b44da91aa344c6d8ae8c9af66cafc
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-05-07 13:36:18 +0200

    Fixed alrighty then... :dancers:

--- MMO/Assets/Scripts/Networking/PlayerCallbacks.cs
+++ MMO/Assets/Scripts/Networking/PlayerCallbacks.cs
@@ -1,12 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
-//[BoltGlobalBehaviour("Scene1Test")]
 [BoltGlobalBehaviour("TutorialLevel", "Scene1TestNew")]
-=======
-[BoltGlobalBehaviour("Scene1TestNew")]
->>>>>>> origin/master
 public class PlayerCallbacks : Bolt.GlobalEventListener
 {
 	public override void SceneLoadLocalDone (string map)
