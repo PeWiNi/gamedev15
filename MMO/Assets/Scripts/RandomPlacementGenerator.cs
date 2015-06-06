3ec892a9406008fd28fbf058ac634a7f01ac596c
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-06-06 17:34:53 +0200

    Started on new Level Design
    Added additional environmental stuff to the beacons.
    Tell what should be different or if something should be changed :)

--- MMO/Assets/Scripts/RandomPlacementGenerator.cs
+++ MMO/Assets/Scripts/RandomPlacementGenerator.cs
@@ -13,14 +13,23 @@ public class RandomPlacementGenerator : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		
 		for (int i = 0; i < noOfInstances; i++) {
 			GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
-			fab.transform.position = new Vector3 (Random.Range (terrainWidth / 2, - terrainWidth / 2), 2f, Random.Range (terrainLength / 2, -terrainLength / 2));
+			fab.transform.position = new Vector3 (Random.Range (gameObject.transform.position.x + (gameObject.transform.localScale.x / 2), gameObject.transform.position.x - (gameObject.transform.localScale.x / 2)), 
+			                                      0.1f, 
+			                                      Random.Range (gameObject.transform.position.z + (gameObject.transform.localScale.z / 2), gameObject.transform.position.z - (gameObject.transform.localScale.z / 2)));
 			if(scaleX != 0 && scaleY != 0 && scaleY !=0) {
 				fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
 			}
+			fab.transform.parent = gameObject.transform;
 		}
-		Destroy(gameObject);
+//		for (int i = 0; i < noOfInstances; i++) {
+//			GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+//			fab.transform.position = new Vector3 (Random.Range (terrainWidth / 2, - terrainWidth / 2), 0.1f, Random.Range (terrainLength / 2, -terrainLength / 2));
+//			if(scaleX != 0 && scaleY != 0 && scaleY !=0) {
+//				fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
+//			}
+//		}
+//		Destroy(gameObject);
 	}
 }
