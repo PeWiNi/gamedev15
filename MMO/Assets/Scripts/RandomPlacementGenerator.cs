7b3b65efc447787d6f00940c5f3356d87b86ca8e
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-06-11 14:11:57 +0200

    Additional changes made to the new Level Design
    Borders Near Lake (InProgress).
    Textured the Level (Tell me what should be different, perhaps the ground
    road texture should be removed?)
    Added some more stuff around the Lake.
    
    NEXT STEP - Thought about trying to add the hidy grass next, and place
    the power ups strategically :)

--- MMO/Assets/Scripts/RandomPlacementGenerator.cs
+++ MMO/Assets/Scripts/RandomPlacementGenerator.cs
@@ -10,26 +10,42 @@ public class RandomPlacementGenerator : MonoBehaviour {
 	public float scaleX;
 	public float scaleY;
 	public float scaleZ;
+	public bool isBorder = true;// Whether or not it is an edge of the world.
+	public bool isLocalised = true;// Set to false if we want to expand it all over the terrain area.
 	
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < noOfInstances; i++) {
-			GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
-			fab.transform.position = new Vector3 (Random.Range (gameObject.transform.position.x + (gameObject.transform.localScale.x / 2), gameObject.transform.position.x - (gameObject.transform.localScale.x / 2)), 
-			                                      0.1f, 
-			                                      Random.Range (gameObject.transform.position.z + (gameObject.transform.localScale.z / 2), gameObject.transform.position.z - (gameObject.transform.localScale.z / 2)));
-			if(scaleX != 0 && scaleY != 0 && scaleY !=0) {
-				fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
+		if (isLocalised) {
+			for (int i = 0; i < noOfInstances; i++) {
+				GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+				if (isBorder) {
+					fab.transform.position = new Vector3 (Random.Range (gameObject.transform.position.x + (gameObject.transform.localScale.x / 2), gameObject.transform.position.x - (gameObject.transform.localScale.x / 2)), 
+				                                      0.1f, 
+				                                      Random.Range (gameObject.transform.position.z + (gameObject.transform.localScale.z / 2), gameObject.transform.position.z - (gameObject.transform.localScale.z / 2)));
+				}
+				if (!isBorder) {
+					fab.transform.position = new Vector3 (Random.Range (0f, gameObject.GetComponent<BoxCollider> ().bounds.center.x), 
+				                                      0.1f, 
+					                                      Random.Range (0f, gameObject.GetComponent<BoxCollider> ().bounds.center.z));
+					fab.transform.rotation = gameObject.transform.rotation;//set the same rotation as this gameObject.
+					Debug.Log ("Fab " + fab + ", Center x = " + gameObject.GetComponent<BoxCollider> ().bounds.center.x);
+					Debug.Log ("Fab " + fab + ", Center z = " + gameObject.GetComponent<BoxCollider> ().bounds.center.z);
+				}
+				if (scaleX != 0 && scaleY != 0 && scaleY != 0) {
+					fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
+				}
+				fab.transform.parent = gameObject.transform;
 			}
-			fab.transform.parent = gameObject.transform;
+		} else {
+			for (int i = 0; i < noOfInstances; i++) {
+				GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+				fab.transform.position = new Vector3 (Random.Range (terrainWidth / 2, - terrainWidth / 2), 0.1f, Random.Range (terrainLength / 2, -terrainLength / 2));
+				if (scaleX != 0 && scaleY != 0 && scaleY != 0) {
+					fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
+				}
+				fab.transform.parent = gameObject.transform;
+			}
 		}
-//		for (int i = 0; i < noOfInstances; i++) {
-//			GameObject fab = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
-//			fab.transform.position = new Vector3 (Random.Range (terrainWidth / 2, - terrainWidth / 2), 0.1f, Random.Range (terrainLength / 2, -terrainLength / 2));
-//			if(scaleX != 0 && scaleY != 0 && scaleY !=0) {
-//				fab.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
-//			}
-//		}
 //		Destroy(gameObject);
 	}
 }
